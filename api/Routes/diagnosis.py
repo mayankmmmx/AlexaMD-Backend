5985b8425313863184adddca76bee84c29ce16fa
Mayank Makwana <mayankmmmx@gmail.com>
2016-09-25 09:55:53 -0400

    changed api keys

--- api/Routes/diagnosis.py
+++ api/Routes/diagnosis.py
@@ -20,7 +20,7 @@ def create_post_data(symptom_ids, age, sex):
 
 def get_diagnoses(evidence):
 	POST_URL = "https://api.infermedica.com/v2/diagnosis"
-	header = { "app_id" : "9cfcafae", "app_key" : "55b78a933718c8e68ba37f2f8d80b1a7", "Content-Type": "application/json"}
+	header = { "app_id" : "cfcc432c", "app_key" : "5f889c7c9166c45cbc8d3ea180493897", "Content-Type": "application/json"}
 	response = requests.post(POST_URL, headers=header, data=evidence)
 	diagnoses = response.json().get("conditions")
 
