af3883d977f52f6aaebcab73cf722702c0fc16aa
Mayank Makwana <mayankmmmx@gmail.com>
2016-09-25 06:12:01 -0400

    Configuring AWS

--- api/application.py
+++ api/application.py
@@ -1,6 +1,6 @@
 # hack for harambe
 from flask import Flask, Response, request, jsonify
-import Routes.diagnosis
+#import Routes.diagnosis
 application = Flask(__name__)
 
 
@@ -9,11 +9,11 @@ application = Flask(__name__)
 def index():
     return "Welcome to AlexaMD's RESTful API!!!"
 
-
+'''
 @application.route('/harambe/diagnosis', methods=['POST'])
 def route_diagnosis():
     if request.headers['Content-Type'] == 'application/json':
         return jsonify(Routes.diagnosis.respond(request.get_json()))
-
+'''
 if __name__ == "__main__":
     application.run(debug=True)
